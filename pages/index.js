d0d7c429a2fca1723728ba12bfd5eb83b2a08d45
David Barel <david@avalanche.codes>
2022-10-04 20:01:27 -0700

    fix cta

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
 
 
         <section className="pt-0 position-relative">
-            <ProjectCta />
+            {/*<ProjectCta />*/}
         </section>
 
     </>
